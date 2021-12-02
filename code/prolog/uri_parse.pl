3e045a19526658e4c9e4704f90a866d0136f47c3
Francesco Barbieri <fbarbieri.business@gmail.com>
2021-12-02 23:09:50 +0100

    riconoscimento e output dello scheme, correzione cut e .

--- code/prolog/uri_parse.pl
+++ code/prolog/uri_parse.pl
@@ -1,4 +1,4 @@
-uri_parse (URIString, URI) :-
+uri_parse(URIString, URI) :-
     %conversione da stringa a lista di codici
     string_to_list(URIString, URICodeList),
 
@@ -7,9 +7,22 @@ uri_parse (URIString, URI) :-
 
     %se trovo un ":" nella lista di char non faccio backtracking dato che è l'elemento che mi indica la fine dello scheme
     member(':', URIList),
-    !.
+    !,
 
-    URI = uri(Scheme, Userinfo, Host, Port, Path, Query, Fragment).
+	%divido la stringa URIList in due parti con il predicato splitString, spiegato dopo. Questo per identificare lo scheme e "separarlo" dal resto della stringa
+	splitList(URIList, :, Scheme, Sottostringa),
+
+	%a partire da Scheme genero una stringa SchemeOut da poter stampare a video
+	out_scheme(Scheme, SchemeOut),
+	
+    URI = uri(SchemeOut). %, Userinfo, Host, Port, Path, Query, Fragment).
+
+uri(_, _, _, _, _, _, _).
+
+%genero una stringa SchemeOut da poter stampare a video
+out_scheme(Scheme, SchemeOut) :- 
+	verifica_identificatori(Scheme),
+	string_to_atom(Scheme, SchemeOut).
 
 %convertitore da codici dei caratteri in caratteri leggibili
 codeListToAtomList([], []) :- !.
@@ -17,7 +30,7 @@ codeListToAtomList([X | Xs], [Y | Ys]) :-
 	char_code(Y, X), 
 	codeListToAtomList(Xs, Ys).
 
-%funzione che data una stringa, ritorna true se trova un identificatore
+%predicato che data una stringa, ritorna true se trova un identificatore
 verifica_identificatori(X) :- 
 	length(X, 1),  %True se la lista X contiene 1 elemento
 	nth0(0, X, Y), %True se l'elemento Y alla posizione 0 della lista X è uno sei deguenti caratteri
@@ -35,23 +48,9 @@ verifica_identificatori([X | Xs]) :-
 	X \= ':',
 	verifica_identificatori(Xs).
 
-verifica_identificatori_host(X) :- 
-	length(X, 1),
-	nth0(0, X, Y),
-	Y \= '.', 
-	Y \= '/', 
-	Y \= '?', 
-	Y \= '#', 
-	Y \= '@',
-    Y \= ':',
+%predicato splitList, per dividere una stringa data in una parte prima e dopo un carattere dato
+splitList([A|Ls], A, [], Ls) :- !.
+splitList([L|Ls], A, [L|Xs], R):- 
+	L\==A,
+	splitList(Ls, A, Xs, R),
 	!.
-verifica_identificatori_host([X | Xs]) :-
-	Y \= '.', 
-	Y \= '/', 
-	Y \= '?', 
-	Y \= '#', 
-	Y \= '@',
-    Y \= ':',
-	verifica_identificatori_host(Xs).
-
-verifica_scheme(X) :- verifica_identificatori(X).
