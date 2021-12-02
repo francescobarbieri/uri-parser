6e8772ac3dc366e15199cd9b2ebfc2eb3a4f634e
federico bartsch <f.bartsch@campus.unimib.it>
2021-12-02 16:50:57 +0100

    aggiunta file bartsch

--- code/prolog/uri_parser_bartsch.pl
+++ code/prolog/uri_parser_bartsch.pl
@@ -2,10 +2,20 @@ uri_parse(URIString, URI):-
 	string_to_list(URIString, URICodeList),
 	codeListToAtomList(URICodeList, URIList),
 	member(':', URIList),
+	splitList(URIList, ':', X, Y),
+	append(X, ':', Scheme),
 	!.
+
+
 
 
 codeListToAtomList([], []) :- !.
 codeListToAtomList([X | Xs], [Y | Ys]) :- 
  	char_code(Y, X), 
  	codeListToAtomList(Xs, Ys).
+
+
+splitList([A|Ls], A, [], Ls).
+splitList([L|Ls], A, [L|Xs], R):- 
+		L\==A,
+		splitList(Ls, A, Xs, R).
