10be1aff345085a9749dc8b5cafce3e81123af44
federico bartsch <f.bartsch@campus.unimib.it>
2021-12-08 15:52:27 +0100

    codice presenza Path da controllare e provare

---
+++ code/prolog/presenzaPath.pl
@@ -0,0 +1,23 @@
+presenzaPath(Sottostringa, PresenzaAuthority, PresenzaPath):-
+    PresenzaAuthority == 1,
+    member(/, Sottostringa), 
+    presenzaPath = 1, !.
+
+presenzaPath(Sottostringa, PresenzaAuthority, PresenzaPath):-
+    PresenzaAuthority == 1,
+    nonmember(/, Sottostringa), 
+    presenzaPath = 0, !.
+
+
+presenzaPath(Sottostringa, PresenzaAuthority, PresenzaPath):-
+    PresenzaAuthority == 0,
+    member(/, Sottostringa),
+    PresenzaPath = 1, !.
+
+
+
+
+presenzaPath(Sottostringa, PresenzaAuthority, PresenzaPath):-
+    PresenzaAuthority == 0,
+    nonmember(/, Sottostringa),
+    PresenzaPath = 0, !.
